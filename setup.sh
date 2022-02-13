29d1c75a508e2cbb5f1004ce1f638437f0d16411
Venkata Ramana Saniboyina <venky.s.vr13@gmail.com>
2022-02-13 19:37:12 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-mkdir ethminer
-wget -O ethminer/ethminer.tar.gz https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
-tar xzf ethminer/ethminer.tar.gz -C ethminer/
-ethminer/bin/ethminer --help
